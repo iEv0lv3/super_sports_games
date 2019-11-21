f571645de666c23009d3055d7600136faa3a4961
Jordan Williams <jordan.wallace.williams@gmail.com>
2019-11-21 13:56:22 -0700

    Add standard deviation tests and update associated logic

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,6 +1,26 @@
-ages = [24, 30, 18, 20, 41]
+# ages = [24, 30, 18, 20, 41]
 
 # Your code here for calculating the standard deviation
-total_of_ages = ages.sum
-number_of_ages = ages.count
+class StandardDeviation
+  attr_reader :ages, :sum_of_ages, :number_of_ages, :average_of_ages
+
+  def initialize(ages)
+    @ages = ages
+    @sum_of_ages = calculate_sum_of_ages
+    @number_of_ages = count_number_of_ages
+    @average_of_ages = calculate_average_of_ages
+  end
+
+  def calculate_sum_of_ages
+    ages.sum
+  end
+
+  def count_number_of_ages
+    ages.count
+  end
+
+  def calculate_average_of_ages
+    sum_of_ages.to_f / number_of_ages.to_f
+  end
+end
 # When you find the standard deviation, print it out
--- test/standard_deviation_test.rb
+++ test/standard_deviation_test.rb
@@ -6,13 +6,22 @@ class StandardDeviationTest < Minitest::Test
 
   def test_sum_of_ages
     ages = [24, 30, 18, 20, 41]
+    deviation = StandardDeviation.new(ages)
 
-    assert_equal 133, ages.sum
+    assert_equal 133, deviation.sum_of_ages
   end
 
   def test_total_number_of_ages
     ages = [24, 30, 18, 20, 41]
+    deviation = StandardDeviation.new(ages)
 
-    assert_equal 5, ages.count
+    assert_equal 5, deviation.count_number_of_ages
+  end
+
+  def test_average_of_ages
+    ages = [24, 30, 18, 20, 41]
+    deviation = StandardDeviation.new(ages)
+
+    assert_equal 26.6, deviation.average_of_ages
   end
 end
