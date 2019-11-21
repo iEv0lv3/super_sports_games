0c8c95b61db04ced7db9c6a50c45271ec731a087
Jordan Williams <jordan.wallace.williams@gmail.com>
2019-11-21 14:58:15 -0700

    Finish standard deviation tests and associated logic

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,8 +1,7 @@
-# ages = [24, 30, 18, 20, 41]
-
 # Your code here for calculating the standard deviation
 class StandardDeviation
-  attr_reader :ages, :sum_of_ages, :number_of_ages, :average_of_ages, :subtracted_ages, :squared_numbers
+  attr_accessor :ages
+  attr_reader :sum_of_ages, :number_of_ages, :average_of_ages, :subtracted_ages, :squared_numbers, :squared_numbers_sum, :squared_numbers_average, :answer
 
   def initialize(ages)
     @ages = ages
@@ -11,6 +10,8 @@ class StandardDeviation
     @average_of_ages = calculate_average_of_ages
     @subtracted_ages = subtract_each_integer_by_average
     @squared_numbers = square
+    @squared_numbers_sum = squared_sum
+    @squared_numbers_average = average_squared_sum
   end
 
   def calculate_sum_of_ages
@@ -40,5 +41,21 @@ class StandardDeviation
     end
     squared_numbers
   end
+
+  def squared_sum
+    squared_numbers.sum.round(1)
+  end
+
+  def average_squared_sum
+    (self.squared_numbers_sum / self.number_of_ages).round(2)
+  end
+
+  def answer
+    Math.sqrt(squared_numbers_average).round(2)
+  end
 end
 # When you find the standard deviation, print it out
+
+ages = [24, 30, 18, 20, 41]
+deviation = StandardDeviation.new(ages)
+p deviation.answer
--- test/standard_deviation_test.rb
+++ test/standard_deviation_test.rb
@@ -39,4 +39,25 @@ class StandardDeviationTest < Minitest::Test
 
     assert_equal [6.76, 11.56, 73.96, 43.56, 207.36], deviation.squared_numbers
   end
+
+  def test_sum_of_squared_numbers
+    ages = [24, 30, 18, 20, 41]
+    deviation = StandardDeviation.new(ages)
+
+    assert_equal 343.2, deviation.squared_sum
+  end
+
+  def test_average_of_squared_numbers
+    ages = [24, 30, 18, 20, 41]
+    deviation = StandardDeviation.new(ages)
+
+    assert_equal 68.64, deviation.squared_numbers_average
+  end
+
+  def test_answer
+    ages = [24, 30, 18, 20, 41]
+    deviation = StandardDeviation.new(ages)
+
+    assert_equal 8.28, deviation.answer
+  end
 end
